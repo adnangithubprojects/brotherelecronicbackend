ab1baf327f92b0c8f9bfea80dffb5b0cfd40906c
Sanan <Muhammad.Sanan@afiniti.com>
2022-12-24 14:54:12 +0500

    ok

--- cloudinary.js
+++
@@ -1,9 +0,0 @@
-const Ccloudinary= require("cloudinary")
-Ccloudinary.config({
-  cloud: {
-    cloudName: process.env.CLOUD_NAME,
-    apikey: process.env.API_KEY,
-    apiscret: process.env.API_SECRET
-
-  }
-});
